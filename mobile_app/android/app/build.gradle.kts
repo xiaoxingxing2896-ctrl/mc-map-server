c12a4d843f0a67de4dad5f9ecdc75b301d155ae3
文星 肖 <xwx200896@outlook.com>
2026-08-28 09:11:17 +0800

    chore(app): release signing with xiaoxingstar keystore

--- mobile_app/android/app/build.gradle.kts
+++ mobile_app/android/app/build.gradle.kts
@@ -15,25 +15,27 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "dev.mcmap.mc_server_map"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        // Uses the version code from pubspec.yaml. When using split APKs, 1000 * ABI_VERSION
-        // is added automatically by Flutter. (https://developer.android.com/studio/build/configure-apk-splits#configure-APK-versions)
-        // You can force using the value of versionCode by specifying the `-P force-version-code-ignoring-abi=true`
-        // flag during build.
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            // xiaoxingstar 签名（keystore: H:\dsh\mmap\keystore\xiaoxingstar.jks）
+            keyAlias = "xiaoxingstar"
+            keyPassword = "xiaoxingstar2026"
+            storeFile = file("H:/dsh/mmap/keystore/xiaoxingstar.jks")
+            storePassword = "xiaoxingstar2026"
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // 生产签名：xiaoxingstar
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
